f05a8f0885fcdf02bd50629ad9f2070e57aae898
Stephen Cresswell <stephen.cresswell@guidesmiths.com>
2016-06-19 17:48:27 +0100

    Adding simple example

---
+++ examples/simple/index.js
@@ -0,0 +1,26 @@
+var Rascal = require('../..')
+var config = require('./config')
+
+Rascal.Broker.create(Rascal.withDefaultConfig(config), function(err, broker) {
+
+  if (err) throw err
+  broker.on('error', console.error)
+
+  broker.subscribe('demo_sub', function(err, subscription) {
+    if (err) throw err
+    subscription.on('message', function(message, content, ackOrNack) {
+      console.log(content)
+      ackOrNack()
+    })
+    subscription.on('error', console.error)
+  })
+
+  setInterval(function() {
+    broker.publish('demo_pub', new Date().toISOString() + ': hello world', function(err, publication) {
+      if (err) throw err
+      publication.on('error', console.error)
+    })
+  }, 1000)
+})
+
+
