139ba717fe130613983ecd456f59ce1f1eace921
Stephen Cresswell <229672+cressie176@users.noreply.github.com>
2025-12-23 10:51:13 +0000

    Refactor broker subscription error handling

--- examples/default-exchange/index.js
+++ examples/default-exchange/index.js
@@ -4,17 +4,16 @@ var config = require('./config');
 Rascal.Broker.create(Rascal.withDefaultConfig(config), function (err, broker) {
   if (err) throw err;
 
-  broker
-    .subscribe('demo_sub', function (err, subscription) {
-      if (err) throw err;
-      subscription
-        .on('message', function (message, content, ackOrNack) {
-          console.log(content);
-          ackOrNack();
-        })
-        .on('error', console.error);
-    })
-    .on('error', console.error);
+  broker.subscribe('demo_sub', function (err, subscription) {
+    if (err) throw err;
+    subscription
+      .on('message', function (message, content, ackOrNack) {
+        console.log(content);
+        ackOrNack();
+      })
+      .on('error', console.error);
+  });
+  broker.on('error', console.error);
 
   setInterval(function () {
     broker.publish('demo_pub', new Date().toISOString() + ': hello world', 'demo_q', function (err, publication) {
